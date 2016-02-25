3118209fbfee22b53e7b357a7c8296d0e31adfce
Andrew <aschulak@gmail.com>
2016-02-25 17:03:32 -0500

    spelling error

--- lobot/logic/messages.py
+++ lobot/logic/messages.py
@@ -3,6 +3,6 @@ MESSAGES['lobot'] = {}
 MESSAGES['lobot']['1'] = 'Make up an emergency and come get me out of this meeting ASAP!'
 MESSAGES['lobot']['2'] = 'I have an exciting growth opportunity for you! Stop on by and I\'ll tell you all about it.'
 MESSAGES['lobot']['3'] = 'Put your hand down. You do NOT have any questions to ask.'
-MESSAGES['lobot']['4'] = 'Immediately fake a heart attack and lie on the ground until the paramdeics arrive.'
+MESSAGES['lobot']['4'] = 'Immediately fake a heart attack and lie on the ground until the paramedics arrive.'
 MESSAGES['lobot']['5'] = 'Order 45. It works EVERY time.'
 MESSAGES['lobot']['6'] = 'You\'re fired.'
