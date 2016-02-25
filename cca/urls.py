f3ece55c709c4f3bfa028a9c7fe65b571c87192d
Andrew <aschulak@gmail.com>
2016-02-25 10:58:25 -0500

    add badger urls

--- cca/urls.py
+++ cca/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^lobot/', include('lobot.urls')),
-    #url(r'^badger/', include('badger.urls')),
+    url(r'^badger/', include('badger.urls')),
 ]
