3d03e24a6993aed3ca40cb282266c3587f5df5e7
Andrew <aschulak@gmail.com>
2016-02-25 10:41:10 -0500

    models init

---
+++ badger/models/__init__.py
@@ -0,0 +1,27 @@
+from badge import Badge
+
+def model_encode(obj):
+    enc = None
+    try:
+        enc = obj.__json__()
+    except:
+        logging.exception('Error encoding')
+        enc = None
+
+    if enc:
+        return enc
+
+    raise TypeError(repr(obj) + " is not JSON serializable")
+
+def model_encode_verbose(obj):
+    enc = None
+    try:
+        enc = obj.__json_verbose__()
+    except Exception, e:
+        logging.exception('Error encoding verbose')
+        enc = None
+
+    if enc:
+        return enc
+
+    raise TypeError(repr(obj) + " is not JSON serializable")
