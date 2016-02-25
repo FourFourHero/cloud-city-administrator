3b33284777a46266430541e17918cab54435fb64
Andrew <aschulak@gmail.com>
2016-02-25 10:52:16 -0500

    settings and bug fix

--- cca/settings.py
+++ cca/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'push_notifications',
-    #'badger',
+    'badger',
     'lobot'
 ]
 
--- lobot/logic/messages.py
+++ lobot/logic/messages.py
@@ -1,7 +1,7 @@
 MESSAGES = {}
 MESSAGES['lobot'] = {}
 MESSAGES['lobot']['1'] = 'Pretend there is a production issue and come get me out of this meeting ASAP!'
-MESSAGES['lobot']['2'] = 'I have an exciting grwoth opportunity for you! Stop on by and I\'ll tell you all about it.
+MESSAGES['lobot']['2'] = 'I have an exciting grwoth opportunity for you! Stop on by and I\'ll tell you all about it.'
 MESSAGES['lobot']['3'] = 'Put your hand down. You do NOT have any questions to ask.'
 MESSAGES['lobot']['4'] = 'Immediately fake a heart attack and lie on the ground until the paramdeics arrive.'
 MESSAGES['lobot']['5'] = 'Order 45. It works EVERY time.'
